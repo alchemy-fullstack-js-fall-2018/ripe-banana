1826f2fb01c76cd9b5baa003e33a26ac3ee06cf6
KLPainter <KLPainter@gmail.com>
2018-10-12 11:58:23 -0700

    problem with 'any' validation

--- test/e2e/actors.test.js
+++ test/e2e/actors.test.js
@@ -1,8 +1,40 @@
 const request = require('supertest');
 const app = require('../../lib/app');
-//const { dropCollection } = require('./db');
+const { dropCollection } = require('./db');
 
 describe('actors', () => {
+
+    let actors = [
+        {
+            name: 'Matt Diamond',
+            dob: new Date('11-11-1911'),
+            pob: 'Sweden'
+        },
+        {
+            name: 'Susan Surandin',
+            dob: new Date('04-14-1985'),
+            pob: 'Miami'
+        }
+    ];
+
+    let createdActors;
+    
+    const createActor = actor => {
+        return request(app)
+            .post('/actors')
+            .send(actor)
+            .then(res => res.body);
+    };
+
+    beforeEach(() => {
+        return dropCollection('actors');
+    });
+
+    beforeEach(() => {
+        return Promise.all(actors.map(createActor))
+            .then(actorsRes => { createdActors = actorsRes;});
+    });
+
 
     it('creates an actor', () => {
         const newActor = {
