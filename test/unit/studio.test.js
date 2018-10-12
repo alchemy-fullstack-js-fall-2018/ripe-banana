a523f1bb40c7f1314ed822f804366bdf9f39f520
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-12 11:19:06 -0700

    studio model validated

---
+++ test/unit/studio.test.js
@@ -0,0 +1,32 @@
+const Studio = require('../../lib/models/Studio');
+const { getErrors } = require('../util/helpers');
+
+describe('studio model', () => {
+    it('validates a model', () => {
+        const data = {
+            name: 'Marvel Studios',
+            address:{
+                city: 'Burbank',
+                state: 'California',
+                country: 'United States'
+            }
+        };
+
+        const studio = new Studio(data);
+        const jsonStudio = studio.toJSON();
+        expect(jsonStudio).toEqual({ ...data, _id: expect.any(Object) });
+    });
+
+    it('requires a name', () => {
+        const studio = new Studio({
+            address:{
+                city: 'Burbank',
+                state: 'California',
+                country: 'United States'
+            }
+        });
+
+        const errors = getErrors(studio.validateSync(), 1);
+        expect(errors.name.properties.message).toEqual('Path `name` is required.');
+    });
+});
