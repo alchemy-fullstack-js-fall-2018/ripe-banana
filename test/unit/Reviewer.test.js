a3cf64128872d5bcfbef719989ed564c2e7253d2
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-12 11:23:14 -0700

    reviewer validation tests green

---
+++ test/unit/Reviewer.test.js
@@ -0,0 +1,26 @@
+const { getErrors } = require('./helpers');
+const Reviewer = require('../../lib/models/Reviewer');
+
+describe('Reviewer Model', () => {
+    it('returns the Reviewer Schema Model', () => {
+        const data = {
+            name: 'Roger Ebert',
+            company: 'Two Thumbs Up'
+        };
+
+        const reviewer = new Reviewer(data);
+        const jsonReviewer = reviewer.toJSON();
+        expect(jsonReviewer).toEqual({ ...data, _id: expect.any(Object) });
+    });
+
+    it('validates that a name has been passed', () => {
+        const reviewer = new Reviewer({
+            company: 'Two Thumbs Up'
+        });
+
+        const errors = getErrors(reviewer.validateSync(), 1);
+        expect(errors.name.properties.message).toEqual('Path `name` is required.');
+    });
+});
+
+
