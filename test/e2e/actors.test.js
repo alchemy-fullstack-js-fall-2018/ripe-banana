c6fd6ae631507b21b2cfbe0b20efb1247483ae34
KLPainter <KLPainter@gmail.com>
2018-10-12 12:12:30 -0700

    first actor test passing (again)

--- test/e2e/actors.test.js
+++ test/e2e/actors.test.js
@@ -4,42 +4,42 @@ const { dropCollection } = require('./db');
 
 describe('actors', () => {
 
-    let actors = [
-        {
-            name: 'Matt Diamond',
-            dob: new Date('11-11-1911'),
-            pob: 'Sweden'
-        },
-        {
-            name: 'Susan Surandin',
-            dob: new Date('04-14-1985'),
-            pob: 'Miami'
-        }
-    ];
+    // let actors = [
+    //     {
+    //         name: 'Matt Diamond',
+    //         dob: new Date('11-11-1911'),
+    //         pob: 'Sweden'
+    //     },
+    //     {
+    //         name: 'Susan Surandin',
+    //         dob: new Date('04-14-1985'),
+    //         pob: 'Miami'
+    //     }
+    // ];
 
-    let createdActors;
+    // let createdActors;
     
-    const createActor = actor => {
-        return request(app)
-            .post('/actors')
-            .send(actor)
-            .then(res => res.body);
-    };
+    // const createActor = actor => {
+    //     return request(app)
+    //         .post('/actors')
+    //         .send(actor)
+    //         .then(res => res.body);
+    // };
 
-    beforeEach(() => {
-        return dropCollection('actors');
-    });
+    // beforeEach(() => {
+    //     return dropCollection('actors');
+    // });
 
-    beforeEach(() => {
-        return Promise.all(actors.map(createActor))
-            .then(actorsRes => { createdActors = actorsRes;});
-    });
+    // beforeEach(() => {
+    //     return Promise.all(actors.map(createActor))
+    //         .then(actorsRes => { createdActors = actorsRes;});
+    // });
 
 
     it('creates an actor', () => {
         const newActor = {
             name: 'Sir Clancy Yorkshire IV',
-            dob: new Date('12-12-1934'),
+            dob: '1934-12-12T08:00:00.000Z',
             pob: 'New Jersey'
         };
         return request(app)
