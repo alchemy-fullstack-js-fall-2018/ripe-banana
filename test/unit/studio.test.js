8d309278ce4fa403012aa2f6de9eae04b438b42a
Ryan Luras <ryanluras@gmail.com>
2018-10-12 09:38:18 -0700

    notes from morning meet

--- test/unit/studio.test.js
+++ test/unit/studio.test.js
@@ -18,3 +18,10 @@ describe('Studio Model', () => {
     });
 
 });
+
+
+//start with Studio, Actors, Reviewer, (thses do not refrence other models)
+//write model, route and test, vertical slices
+//then make Film which refrences Studio and Actors(cast array)
+//then => Review relies on Film and Reviewers
+
