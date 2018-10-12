5260d85747abba8a7eeef6b15622e31f36de7e52
Sophie Graham <sophiergraham1@gmail.com>
2018-10-12 16:47:01 -0700

    actors post read

--- test/e2e/actors.test.js
+++ test/e2e/actors.test.js
@@ -1,1 +1,29 @@
-describe.skip();
+require('dotenv').config();
+require('../../lib/util/connect')();
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../../lib/app');
+// const Actor = require('../../lib/models/Actor');
+
+describe('actors route', () => {
+    it('creates an actor', () => {
+        return request(app).post('/api/actors')
+            .send({
+                name: 'Keanu Reeves',
+                dob: new Date('September 2, 1964'),
+                pob: 'Beirut, Lebanon'
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    __v: expect.any(Number),
+                    name: 'Keanu Reeves',
+                    dob: 'September 2, 1964',
+                    pob: 'Beirut, Lebanon'          
+                });
+            });
+            
+
+    });
+});
+
