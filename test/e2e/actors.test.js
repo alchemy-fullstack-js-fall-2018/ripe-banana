76978e9a99ef125b7ec32c797671a8060a7370b1
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-12 17:13:43 -0700

    actors getall red

--- test/e2e/actors.test.js
+++ test/e2e/actors.test.js
@@ -3,7 +3,49 @@ require('../../lib/util/connect')();
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../lib/app');
-// const Actor = require('../../lib/models/Actor');
+const Actor = require('../../lib/models/Actor');
+
+let actors = [
+    {
+        name: 'Nicolas Cage',
+        dob: 'January 7, 1964',
+        pob: 'Long Beach, CA'
+    },
+    {
+        name: 'Bradley Cooper',
+        dob: 'January 5, 1975',
+        pob: 'Philadelphia, PA'
+    },
+    {
+        name: 'Fran Drescher',
+        dob: 'September 30, 1957',
+        pob: 'New York, NY'
+    }
+];
+
+let createdActors;
+
+const createActor = actor => {
+    return request(app)
+        .post('/api/actors/')
+        .send(actor)
+        .then(res => res.body);
+};
+
+beforeEach(() => {
+    return Actor.deleteMany();
+});
+
+beforeEach(() => {
+    return Promise.all(actors.map(createActor))
+        .then(actorsRes => {
+            createdActors = actorsRes;
+        });
+});
+
+afterAll(() => {
+    mongoose.disconnect();
+});
 
 describe('actors route', () => {
     it('creates an actor', () => {
@@ -22,8 +64,15 @@ describe('actors route', () => {
                     pob: 'Beirut, Lebanon'          
                 });
             });
-            
-
     });
+
+    it('gets all actors', () => {
+        return request(app).get('/api/actors')
+            .then(retrievedActors => {
+                createdActors.forEach(createdActor => {
+                    expect(retrievedActors.body).toContainEqual(createdActor);
+                });
+            });
+    })
 });
 
