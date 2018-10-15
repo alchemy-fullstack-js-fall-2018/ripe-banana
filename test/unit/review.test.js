62b87eeaf338e40ffd0dcb238a9c7745157ae8ff
Claire Flanagan <claireflanagan@gmail.com>
2018-10-15 15:09:22 -0700

    all review model tests passing

--- test/unit/review.test.js
+++ test/unit/review.test.js
@@ -18,4 +18,19 @@ describe('review model', () => {
         const jsonReview = review.toJSON();
         expect(jsonReview).toEqual({ ...data, _id: expect.any(Object) });
     });
+
+    it('requires rating, reviewer, review, and film', () => {
+        const review = new Review({
+            timestamps: {
+                createdAt: new Date(2014, 4, 15),
+                updatedAt: new Date(2015, 3, 12)
+            }
+        });
+
+        const errors = getErrors(review.validateSync(), 4);
+        expect(errors.rating.properties.message).toEqual('Path `rating` is required.');
+        expect(errors.reviewer.properties.message).toEqual('Path `reviewer` is required.');
+        expect(errors.review.properties.message).toEqual('Path `review` is required.');
+        expect(errors.film.properties.message).toEqual('Path `film` is required.');
+    });
 });
