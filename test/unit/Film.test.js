971386af053e47ff8ed5c6e7fb89297d5970aba4
Sophie Graham <sophiergraham1@gmail.com>
2018-10-15 14:26:18 -0700

    failing film test

--- test/unit/Film.test.js
+++ test/unit/Film.test.js
@@ -15,6 +15,19 @@ describe('film model', () => {
         };
         const film = new Film(data);
         const jsonFilm = film.toJSON();
-        expect(jsonFilm).toEqual({ ...data, _id: expect.any(Object) });
+        expect(jsonFilm).toEqual({ ...data, cast: [{ _id: expect.any(Object), role: 'Benjamin Gates', actor: expect.any(Object) }], _id: expect.any(Object) });
+    });
+
+    it('requires a title and release year', () => {
+        const film = new Film({
+            studio: Types.ObjectId(),
+            cast: [{
+                role: 'Benjamin Gates',
+                actor: Types.ObjectId()
+            }]
+        });
+        const errors = getErrors(film.validateSync(), 2);
+        expect(errors.title.kind).toEqual('required');
+        expect(errors.released.kind).toEqual('required');
     });
 });
