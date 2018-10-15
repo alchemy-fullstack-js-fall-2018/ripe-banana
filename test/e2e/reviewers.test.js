6cbb9f476bf479db88b8ac121a4c2f796b4b65ed
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-15 10:28:59 -0700

    reviewer create red

--- test/e2e/reviewers.test.js
+++ test/e2e/reviewers.test.js
@@ -1,1 +1,23 @@
-describe.skip();
+require('dotenv').config();
+require('../../lib/util/connect')();
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../../lib/app');
+
+describe('reviewers route', () => {
+    it('creates a reviewer', () => {
+        return request(app).post('/api/reviewers')
+            .send({
+                name: 'Tommy Applegate',
+                company: 'notlovingit.com'
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    __v: expect.any(Number),
+                    name: 'Tommy Applegate',
+                    company: 'notlovingit.com'
+                });
+            });
+    })
+});
