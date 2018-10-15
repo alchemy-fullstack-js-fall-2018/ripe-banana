bc19816c66c5d3e22aaa5dc1dd7ff6cc0181fb2e
Sophie Graham <sophiergraham1@gmail.com>
2018-10-15 09:59:45 -0700

    reviewer model validated

--- test/unit/Reviewer.test.js
+++ test/unit/Reviewer.test.js
@@ -1,1 +1,16 @@
-describe.skip();
+const { getErrors } = require('./helpers');
+const Reviewer = require('../../lib/models/Reviewer');
+
+describe('reviewer model', () => {
+    it('validates a good model', () => {
+        const data = {
+            name: 'Bob Miller',
+            company: 'YourScriptSucks.com'
+        };
+
+        const reviewer = new Reviewer(data);
+        const jsonReviewer = reviewer.toJSON();
+        expect(jsonReviewer).toEqual({ ...data, _id: expect.any(Object) });
+    
+    });
+});
