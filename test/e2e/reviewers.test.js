832818f61546a8187f896a18a0f2611911833a55
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-15 10:43:17 -0700

    get reviewers red

--- test/e2e/reviewers.test.js
+++ test/e2e/reviewers.test.js
@@ -3,6 +3,46 @@ require('../../lib/util/connect')();
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../lib/app');
+const Reviewer = require('../../lib/models/Reviewer');
+
+let reviewers = [
+    {
+        name: 'Owen Gleiberman',
+        company: 'Entertainment Weekly'
+    },
+    {
+        name: 'Sheila',
+        company: 'popqueens.com/thatssosheila'
+    },
+    {
+        name: 'Bobby Jones',
+        company: 'welovemovies.net'
+    }
+];
+
+let createdReviewers;
+
+const createReviewer = reviewer => {
+    return request(app)
+        .post('/api/reviewers/')
+        .send(reviewer)
+        .then(res => res.body);
+};
+
+beforeEach(() => {
+    return Reviewer.deleteMany();
+});
+
+beforeEach(() => {
+    return Promise.all(reviewers.map(createReviewer))
+        .then(reviewersRes => {
+            createdReviewers = reviewersRes;
+        });
+});
+
+afterAll(() => {
+    mongoose.disconnect();
+});
 
 describe('reviewers route', () => {
     it('creates a reviewer', () => {
@@ -19,5 +59,14 @@ describe('reviewers route', () => {
                     company: 'notlovingit.com'
                 });
             });
-    })
+    });
+
+    it('gets all reviewers', () => {
+        return request(app).get('/api/reviewers')
+            .then(retrievedReviewers => {
+                createdReviewers.forEach(createdReviewer => {
+                    expect(retrievedReviewers.body).toContainEqual(createdReviewer);
+                });
+            });
+    });
 });
