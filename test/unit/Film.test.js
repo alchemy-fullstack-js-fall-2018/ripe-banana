16b7c1b337a1f14082ab596a4c69e1d758174437
Sophie Graham <sophiergraham1@gmail.com>
2018-10-15 11:31:03 -0700

    started film unit test

--- test/unit/Film.test.js
+++ test/unit/Film.test.js
@@ -1,1 +1,15 @@
-describe.skip();
+const { getErrors } = require('./helpers');
+const Film = require('../../lib/models/Film');
+const Actor = require('../../lib/models/Actor');
+
+describe('film model', () => {
+    const data = {
+        title: 'National Treasure',
+        studio: 'Walt Disney Pictures',
+        released: 2004,
+        cast: [{
+            role: 'Benjamin Gates',
+            actor: actors.find({actors._id: {} })
+        }]
+    }
+});
