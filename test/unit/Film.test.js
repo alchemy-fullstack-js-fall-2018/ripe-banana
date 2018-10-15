8f042fbd9edd9c801f84eccade0f5c875b856ab3
KLPainter <KLPainter@gmail.com>
2018-10-15 10:10:48 -0700

    Film model complete.  First test in progress.

---
+++ test/unit/Film.test.js
@@ -0,0 +1,49 @@
+const Film = require('../../lib/models/Film');
+const Actor = require('../../lib/models/Actor');
+const Studio = require('../../lib/models/Studio');
+const { getErrors } = require('./helpers');
+
+describe('Film model', () => {
+
+    it('validates a good model', () => {
+
+        const studioData = {
+            name: 'Willamette Pictures',
+            address: {
+                city: 'Portland',
+                state: 'OR',
+                country: 'USA'
+            }
+        };
+
+        const actor1Data = {
+            name: 'Wanda Wanderson',
+            dob: new Date('1971-01-01T08:00:00.000Z'),
+            pob: 'Peoria'
+        };
+
+        const actor2Data = {
+            name: 'Peter Peterson',
+            dob: new Date('1972-01-01T08:00:00.000Z'),
+            pob: 'Detroit'
+        };
+
+        const studio = new Studio(studioData);
+        const actor1 = new Actor(actor1Data);
+        const actor2 = new Actor(actor2Data);
+
+        const filmData = {
+            title: 'Code Storm',
+            studio: studio._id,
+            released: 1945,
+            cast: [ 
+                { role: 'Lead Programmer', actor: actor1._id }, 
+                { role: 'Product Manager', actor: actor2._id }
+            ]
+        };
+        
+
+
+    });
+    
+});
