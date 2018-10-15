8c369b23439ad226ad3d68e08751c41931975a57
Claire Flanagan <claireflanagan@gmail.com>
2018-10-15 10:21:54 -0700

    first film model test passing

--- test/unit/film.test.js
+++ test/unit/film.test.js
@@ -1,22 +1,20 @@
 const Film = require('../../lib/models/Film');
 const { getErrors } = require('../util/helpers');
-const Chance = require('chance');
-const chance = new Chance();
+const { Types } = require('mongoose');
 
 describe('film model', () => {
     it('validates a good model', () => {
         const data = {
             title: 'Hot Pursuit', 
-            studio: chance.guid(),
+            studio: Types.ObjectId(),
             released: 2015, 
             cast: [{
                 role: 'Cop', 
-                actor: chance.guid()
+                actor: Types.ObjectId()
             }]
         };
         const film = new Film(data);
         const jsonFilm = film.toJSON();
         expect(jsonFilm).toEqual({ ...data, _id: expect.any(Object) });
-        expect(jsonFilm.cast[0]).toEqual({ ...data.cast[0] });
     });
 });
