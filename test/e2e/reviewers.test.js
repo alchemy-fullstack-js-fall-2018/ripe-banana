c225da3ad0b4d4f9b8f2d75cd461488626c6d9b9
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-16 12:15:30 -0700

    get reviewer by id test fix in progress, test failing

--- test/e2e/reviewers.test.js
+++ test/e2e/reviewers.test.js
@@ -1,10 +1,94 @@
 const { dropCollection } = require('./db');
 const request = require('supertest');
 const app = require('../../lib/app');
+const { Types } = require('mongoose');
 
 
 describe('validates a vertical slice of the Reviewer route', () => {
    
+    beforeEach(() => {
+        return dropCollection('studios');
+    });
+    beforeEach(() => {
+        return dropCollection('actors');
+    });
+    beforeEach(() => {
+        return dropCollection('films');
+    });
+    beforeEach(() => {
+        return dropCollection('reviewers');
+    });
+    beforeEach(() => {
+        return dropCollection('reviews');
+    });
+
+    let createdActors;
+    let createdStudios;
+    let createdFilms;
+    let createdReviewers;
+    let createdReviews;
+
+    let reviews = [
+        {
+            rating: 5,
+            review: 'This is a badass review of of film 0',
+            reviewer: Types.ObjectId(),
+            film: Types.ObjectId()
+            
+        },
+        {
+            rating: 2,
+            review: 'This is mediocre review of of film 1',
+            reviewer: Types.ObjectId(),
+            film: Types.ObjectId()
+            
+        }
+
+    ];
+
+    let actors =  [{
+        name: 'Anna Peel',
+        dob: new Date(),
+        pob: 'Brazil'
+    },
+    {
+        name: 'Lud Orange',
+        dob: new Date(),
+        pob: 'Florida'
+    }];
+
+    let studios =  [{
+        name: 'Compost Cinema', 
+        address: {
+            city: 'Portland',
+            state: 'OR',
+            country: 'United States'
+        }
+    },
+    {
+        name: 'Compost Cinema2', 
+        address: {
+            city: 'Portland2',
+            state: 'OR',
+            country: 'United States'
+        }
+    }];
+
+    let films =  [{
+        title: 'Bladecrawler',
+        released: 1991,
+        cast: [{
+            role: 'lead',
+        }]
+    },
+    {
+        title: 'Bladewalker',
+        released: 1992,
+        cast: [{
+            role: 'Deckard',
+        }]
+    }];
+
     let reviewers =  [{
         name: 'CreatedReviewer1', 
         company: 'CreatedCompany1'
@@ -12,32 +96,79 @@ describe('validates a vertical slice of the Reviewer route', () => {
     {
         name: 'CreatedReviewer2', 
         company: 'CreatedCompany2'
-
-    },
-    {
-        name: 'CreatedReviewer3', 
-        company: 'CreatedCompany3'
-
     }
     ];
-
-    
-    let createdReviewers;
         
+    const createActor = actor => {
+        return request(app)
+            .post('/api/actors')
+            .send(actor)
+            .then(res => res.body);
+    };
+
+    const createStudio = studio => {
+        return request(app)
+            .post('/api/studios')
+            .send(studio)
+            .then(res => res.body);
+    };
+
+    const createFilm = film => {
+        return request(app)
+            .post('/api/films')
+            .send(film)
+            .then(res => res.body);
+    };
+
     const createReviewer = reviewer => {
         return request(app)
             .post('/api/reviewers')
             .send(reviewer)
             .then(res => res.body);
     };
-        
+
+    const createReview = review => {
+        return request(app)
+            .post('/api/reviews')
+            .send(review)
+            .then(res => res.body);
+    };
+
     beforeEach(() => {
-        return dropCollection('reviewers');
+        return Promise.all(actors.map(createActor)).then(actorsRes => {
+            createdActors = actorsRes;
+            films[0].cast[0].actor = createdActors[0]._id;
+            films[1].cast[0].actor = createdActors[1]._id;
+        });
+    });
+
+    beforeEach(() => {
+        return Promise.all(studios.map(createStudio)).then(studiosRes => {
+            createdStudios = studiosRes;
+            films[0].studio = createdStudios[0]._id;
+            films[1].studio = createdStudios[1]._id;
+        });
     });
         
     beforeEach(() => {
+        return Promise.all(films.map(createFilm)).then(filmsRes => {
+            createdFilms = filmsRes;
+            reviews[0].film = createdFilms[0]._id;
+            reviews[1].film = createdFilms[1]._id;
+        });
+    });
+
+    beforeEach(() => {
         return Promise.all(reviewers.map(createReviewer)).then(reviewersRes => {
             createdReviewers = reviewersRes;
+            reviews[0].reviewer = createdReviewers[0]._id;
+            reviews[1].reviewer = createdReviewers[1]._id;
+        });
+    });
+
+    beforeEach(() => {
+        return Promise.all(reviews.map(createReview)).then(reviewsRes => {
+            createdReviews = reviewsRes;
         });
     });
 
@@ -73,11 +204,6 @@ describe('validates a vertical slice of the Reviewer route', () => {
                     name: createdReviewers[1].name,
                     company: createdReviewers[1].company
                 });
-                expect(res.body).toContainEqual({
-                    _id: createdReviewers[2]._id,
-                    name: createdReviewers[2].name,
-                    company: createdReviewers[2].company
-                });
               
             });
     });
@@ -86,7 +212,20 @@ describe('validates a vertical slice of the Reviewer route', () => {
         return request(app)
             .get(`/api/reviewers/${createdReviewers[1]._id}`)
             .then(res => {
-                expect(res.body).toEqual(createdReviewers[1]);
+                expect(res.body).toEqual({
+                    _id: createdReviewers[1]._id,
+                    name: createdReviewers[1].name,
+                    company: createdReviewers[1].company,
+                    reviews: [{
+                        _id: createdReviews[1]._id,
+                        rating: createdReviews[1].rating,
+                        review: createdReviews[1].review,
+                        film: {
+                            _id: createdReviews[1].film,
+                            title: createdFilms[1].title
+                        }
+                    }]
+                });
             });
 
     });
