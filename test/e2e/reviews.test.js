7b3f6b042f1fe651bbb733021ad07167dc73fa62
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-15 18:02:44 -0700

    initial commit

--- test/e2e/reviews.test.js
+++ test/e2e/reviews.test.js
@@ -4,63 +4,10 @@ const request = require('supertest');
 const app = require('../../lib/app');
 const Chance = require('chance');
 const chance = new Chance();
-const { createActorsArr } = require('../util/helpers');
+const { createActors, createStudios, createReviewers, createFilms, createReviews } = require('../util/helpers');
 
 describe('reviews pub/sub API', () => {
-    let films = [
-        {
-            title: chance.word(),
-            released: chance.natural({ min: 1800, max: 9000 }),
-            cast: [{
-                role: chance.word()
-            }]
-        },
-        {
-            title: chance.word(),
-            released: chance.natural({ min: 1800, max: 9000 }),
-            cast: [{
-                role: chance.word()
-            }]
-        },
-        {
-            title: chance.word(),
-            released: chance.natural({ min: 1800, max: 9000 }),
-            cast: [{
-                role: chance.word()
-            }]
-        }
-    ];
-
-    let reviewers = [
-        {
-            name: chance.name({ suffix: true }),
-            company: chance.company()
-        },
-        {
-            name: chance.name({ suffix: true }),
-            company: chance.company()
-        },
-        {
-            name: chance.name({ suffix: true }),
-            company: chance.company()
-        },
-    ];
-
-    let reviews = [
-        {
-            rating: chance.natural({ min:1, max: 5 }),
-            review: chance.sentence({ words: 7 }) 
-        },
-        {
-            rating: chance.natural({ min:1, max: 5 }),
-            review: chance.sentence({ words: 7 }) 
-        },
-        {
-            rating: chance.natural({ min:1, max: 5 }),
-            review: chance.sentence({ words: 7 }) 
-        }
-    ];
-
+    
     beforeEach(() => {
         return dropCollection('films');
     });
@@ -71,61 +18,36 @@ describe('reviews pub/sub API', () => {
         return dropCollection('reviews');
     });
 
-    const createFilm = film => {
-        return request(app)
-            .post('/api/films')
-            .send(film)
-            .then(res => res.body);
-    };
-    const createReviewer = reviewer => {
-        return request(app)
-            .post('/api/reviewers')
-            .send(reviewer)
-            .then(res => res.body);
-    };
-    const createReview = review => {
-        return request(app)
-            .post('/api/reviews')
-            .send(review)
-            .then(res => res.body);
-    };
-
-        
     let createdFilms;
     let createdReviewers;
     let createdReviews;
     let createdActors;
+    let createdStudios;
 
     beforeEach(() => {
-        createdActors = createActorsArr();
-        createdActors.forEach((actor, index) => {
-            films[index].cast[0].actor = actor._id;
-        });
+        createdActors = [];
+        return createActors(3, createdActors);
+       
     });
 
     beforeEach(() => {
-        return Promise.all(films.map(createFilm))
-            .then(filmRes => {
-                createdFilms = filmRes;
-                createdFilms.forEach((film, index) => {
-                    reviews[index].film = film._id;
-                });
-            });
+        createdStudios = [];
+        return createStudios(3, createdStudios);
+    });
+    beforeEach(() => {
+        createdReviewers = [];
+        return createReviewers(3, createdReviewers);
     });
 
     beforeEach(() => {
-        return Promise.all(reviewers.map(createReviewer))
-            .then(reviewerRes => {
-                createdReviewers = reviewerRes;
-                createdReviewers.forEach((reviewer, index) => {
-                    reviews[index].reviewers = reviewers._id;
-                });
-            });
+        createdFilms = [];
+        return createFilms(3, createdFilms);
     });
 
+
     beforeEach(() => {
-        return Promise.all(reviews.map(createReview))
-            .then(reviewRes => createdReviews = reviewRes);
+        createdReviews = [];
+        return createReviews(3, createdReviews);
     });
     
     it('creates a review', () => {
--- test/util/helpers.js
+++ test/util/helpers.js
@@ -1,7 +1,11 @@
-const app = require('../../lib/app');
 const Chance = require('chance');
 const chance = new Chance();
-const request = require('supertest');
+
+const Actor = require('../../lib/models/Actor');
+const Studio = require('../../lib/models/Studio');
+const Reviewer = require('../../lib/models/Reviewer');
+const Film = require('../../lib/models/Film');
+const Review = require('../../lib/models/Review');
 
 const getErrors = (validation, numberExpected) => {
     expect(validation).toBeDefined();
@@ -10,44 +14,98 @@ const getErrors = (validation, numberExpected) => {
     return errors;
 };
 
-const createActorsArr = () => {
+const jsonify = data => JSON.parse(JSON.stringify(data))
 
-    let actors = [
-        {
+const createActors = (count, arr) => {
+    const actorPromises = Array.apply(null, { length: count }).map(() => {
+        return Actor.create({
             name: chance.name({ suffix: true }),
             dob: chance.date(),
-            pob: chance.word()
-        },
-        {
-            name: chance.name({ suffix: true }),
-            dob: chance.date(),
-            pob: chance.word()
-        },
-        {
-            name: chance.name({ prefix: true }),
-            dob: chance.date(),
-            pob: chance.word()
-        },
-
-    ];
-
-    const createActor = actor => {
-        return request(app)
-            .post('/api/actors')
-            .send(actor)
-            .then(res => res.body);
-    };
-
-    let createdActors;
-    
-    Promise.all(actors.map(createActor))
-        .then(actorRes => createdActors = actorRes);
-
-    return createdActors;
-
+            pob: chance.city()
+        });
+    });
+    return Promise.all(actorPromises).then(actors => {
+        arr.push.apply(arr, jsonify(actors));
+        return actors;
+    });
 };
+const createStudios = (count, arr) => {
+    const studioPromises = Array.apply(null, { length: count }).map(() => {
+        return Studio.create({
+            name: chance.animal(),
+            address:{
+                city: chance.string(),
+                state: chance.string(),
+                country: chance.string()
+            }
+        });
+    });
+    return Promise.all(studioPromises).then(studios => {
+        arr.push.apply(arr, jsonify(studios));
+        return studios;
+    });
+};
+
+const createReviewers = (count, arr) => {
+    const reviewerPromise = Array.apply(null, { length: count }).map(() => {
+        return Reviewer.create({
+            name: chance.name({ suffix: true }),
+            company: chance.company()
+        });
+    });
+    return Promise.all(reviewerPromise).then(reviewers => {
+        arr.push.apply(arr, jsonify(reviewers));
+        return reviewers;
+    });
+};
+
+const createFilms = (count, arr) => {
+    return Promise.all([createActors(count, [], createStudios(count, []))])
+        .then(([actors, studios]) => {
+            const filmPromise = Array.apply(null, { length: count }).map((item, index) => {
+                return Film.create({
+                    title: 'Hot Pursuit', 
+                    studio: studios[index]._id,
+                    released: 2015, 
+                    cast: [{
+                        role: 'Cop', 
+                        actor: actors[index]._id
+                    }]
+                });
+            });
+            return Promise.all(filmPromise).then(films => {
+                arr.push.apply(arr, jsonify(films));
+                return films;
+            });
+        });
+};
+
+const createReviews = (count, arr) => {
+    return Promise.all([createFilms(count, [], createReviewers(count, []))])
+        .then(([films, reviewers]) => {
+            const filmPromise = Array.apply(null, { length: count }).map((item, index) => {
+                return Review.create({
+                    rating: 1,
+                    reviewer: reviewers[index]._id,
+                    review: 'Horrible movie, why not',
+                    film: films[index]._id
+                });
+            });
+            return Promise.all(filmPromise).then(films => {
+                arr.push.apply(arr, jsonify(films));
+                return films;
+            });
+        });
+};
+
+
+
 
 module.exports = {
     getErrors,
-    createActorsArr
+    createActors,
+    createStudios,
+    createReviewers,
+    createFilms, 
+    createReviews
 };
