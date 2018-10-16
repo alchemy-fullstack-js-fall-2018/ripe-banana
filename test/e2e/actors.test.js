7944b1a9aa74a473ef1b0bf45c33f74107d673ad
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-16 13:00:05 -0700

    cleanup

--- test/e2e/actors.test.js
+++ test/e2e/actors.test.js
@@ -160,22 +160,3 @@ describe('validates a vertical slice of the Actor route', () => {
     });
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//start with Studios, Actors, Reviewer, (thses do not refrence other models)
-//write model, route and test, vertical slices
-//then make Film which refrences Studio and Actors(cast array)
-//then => Review relies on Film and Reviewers
-
