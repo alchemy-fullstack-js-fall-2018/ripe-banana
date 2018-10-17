a8832297da8f854b7b95fc36d023c13f0dc87bfb
MikeBLambert <mlamb128@gmail.com>
2018-10-17 10:40:36 -0700

    Get reviewer by id includes films

--- test/e2e/reviewers.test.js
+++ test/e2e/reviewers.test.js
@@ -1,14 +1,38 @@
 const request = require('supertest');
 const app = require('../../lib/app');
 const { dropCollection } = require('./db');
-const { createReviewers } = require('./helpers');
+const { createReviewers, createActors, createStudios } = require('./helpers');
 
 describe('reviewers', () => {
 
     let createdReviewers;
+    let createdActors;
+    let createdStudios;
+    let createdFilms;
+    let createdReviews;
+    
+    const createReview = review => {
+        return request(app)
+            .post('/reviews')
+            .send(review)
+            .then(res => res.body);
+    };
+
+    const createFilm = film => {
+        return request(app)
+            .post('/films')
+            .send(film)
+            .then(res => res.body);
+    };
     
     beforeEach(() => {
-        return dropCollection('reviewers');
+        return Promise.all([
+            dropCollection('reviewers'),
+            dropCollection('actors'),
+            dropCollection('studios'),
+            dropCollection('films'),
+            dropCollection('reviews')
+        ]);
     });
     
     beforeEach(() => {
@@ -16,6 +40,67 @@ describe('reviewers', () => {
             .then(reviewersRes => { 
                 createdReviewers = reviewersRes;
             });
+    });
+
+    beforeEach(() => {
+        return createActors()
+            .then(actorsRes => { 
+                createdActors = actorsRes;
+            });
+    });
+
+    beforeEach(() => {
+        return createStudios()
+            .then(studiosRes => { 
+                createdStudios = studiosRes;
+            });
+    });
+
+    beforeEach(() => {
+        let films = [
+            {
+                title: 'The Programinator',
+                studio: createdStudios[0]._id,
+                released: 1984,
+                cast: [
+                    { role: 'Chief Troublemaker', actor: createdActors[0]._id }, 
+                    { role: 'Sidekick', actor: createdActors[1]._id }
+                ]
+            },
+            {
+                title: 'Thelma and Luigi',
+                studio: createdStudios[0]._id,
+                released: 1972,
+                cast: [
+                    { role: 'Thelma', actor: createdActors[1]._id }, 
+                    { role: 'Luigi', actor: createdActors[0]._id }
+                ]
+            }
+        ];
+    
+        return Promise.all(films.map(createFilm))
+            .then(filmsRes => { createdFilms = filmsRes;});
+    });
+
+    beforeEach(() => {
+        let reviews = [
+            {
+                rating: 5,
+                reviewer: createdReviewers[0]._id,
+                text: 'Amazeballs!',
+                film: createdFilms[0]._id 
+            },
+            {
+                rating: 1,
+                reviewer: createdReviewers[1]._id,
+                text: 'I want the last 1.5 hours of my life back.',
+                film: createdFilms[1]._id 
+            }
+        ];
+
+        return Promise.all(reviews.map(createReview))
+            .then(reviewsRes => { createdReviews = reviewsRes;});
+
     });
 
     it('creates a reviewer', () => {
@@ -50,7 +135,20 @@ describe('reviewers', () => {
         return request(app)
             .get(`/reviewers/${id}`)
             .then(retrievedReviewer => {
-                expect(retrievedReviewer.body).toEqual(createdReviewers[0]);
+                expect(retrievedReviewer.body).toEqual({
+                    _id: createdReviewers[0]._id,
+                    name: createdReviewers[0].name,
+                    company: createdReviewers[0].company,
+                    reviews:[{
+                        _id: createdReviews[0]._id,
+                        rating: createdReviews[0].rating,
+                        text: createdReviews[0].text,
+                        film: {
+                            _id: createdFilms[0]._id,
+                            title: createdFilms[0].title
+                        }
+                    }]
+                });
             });
     });
 
