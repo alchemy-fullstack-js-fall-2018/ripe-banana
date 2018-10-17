cdd8ab298557556ae7f11509333d3815c05177e8
MikeBLambert <mlamb128@gmail.com>
2018-10-17 09:30:50 -0700

    Get studio by id test passing with film retrieval included

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -1,14 +1,26 @@
 const request = require('supertest');
 const app = require('../../lib/app');
 const { dropCollection } = require('./db');
-const { createStudios } = require('./helpers');
+const { createStudios, createActors } = require('./helpers');
 
 describe('studio', () => {
 
     let createdStudios;
+    let createdFilms;
+    let createdActors;
+
+    const createFilm = film => {
+        return request(app)
+            .post('/films')
+            .send(film)
+            .then(res => res.body);
+    };
     
     beforeEach(() => {
-        return dropCollection('studios');
+        return Promise.all([
+            dropCollection('studios'),
+            dropCollection('films')
+        ]);
     });
 
     beforeEach(() => {
@@ -16,6 +28,39 @@ describe('studio', () => {
             .then(studiosRes => { 
                 createdStudios = studiosRes;
             });
+    });
+
+    beforeEach(() => {
+        return createActors()
+            .then(actorsRes => { 
+                createdActors = actorsRes;
+            });
+    });
+
+    beforeEach(() => {
+        let films = [
+            {
+                title: 'The Programinator',
+                studio: createdStudios[0]._id,
+                released: 1984,
+                cast: [
+                    { role: 'Chief Troublemaker', actor: createdActors[0]._id }, 
+                    { role: 'Sidekick', actor: createdActors[1]._id }
+                ]
+            },
+            {
+                title: 'Thelma and Luigi',
+                studio: createdStudios[0]._id,
+                released: 1972,
+                cast: [
+                    { role: 'Thelma', actor: createdActors[1]._id }, 
+                    { role: 'Luigi', actor: createdActors[0]._id }
+                ]
+            }
+        ];
+        
+        return Promise.all(films.map(createFilm))
+            .then(filmsRes => { createdFilms = filmsRes;});
     });
    
     it('creates a studio', () => {
@@ -53,7 +98,15 @@ describe('studio', () => {
         return request(app)
             .get(`/studios/${createdStudios[0]._id}`)
             .then(retrievedStudio => {
-                expect(retrievedStudio.body).toEqual(createdStudios[0]);
+                expect(retrievedStudio.body).toEqual({ 
+                    _id: createdStudios[0]._id,
+                    name: createdStudios[0].name,
+                    address: createdStudios[0].address, 
+                    films:[
+                        { _id: createdFilms[0]._id, title: createdFilms[0].title },
+                        { _id: createdFilms[1]._id, title: createdFilms[1].title }
+                    ] 
+                });
             });
 
     });
