56ea8e6156b57c555353eb6495b4ad85e15d05cb
MikeBLambert <mlamb128@gmail.com>
2018-10-18 16:38:07 -0700

    auth.test

---
+++ test/e2e/auth.test.js
@@ -0,0 +1,28 @@
+const app = require('../../lib/app');
+const request = require('supertest');
+const { dropCollection } = require('./db');
+
+describe('auth', () => {
+    it('creates a reviewer/user on signup', () => {
+        const newReviewer = {
+            name: 'Roger Siskel',
+            company: 'At the Movies',
+            email: 'mrrogers@siskel.com',
+            roles: ['admin'],
+            clearPassword: 'movies123'
+
+        };
+        return request(app)
+            .post('/auth/signup')
+            .send(newReviewer)
+            .then(result => {
+                expect(result.body).toEqual({
+                    name: newReviewer.name,
+                    company: newReviewer.company,
+                    email: newReviewer.email,
+                    roles: newReviewer.roles,
+                    _id: expect.any(String)
+                });
+            });
+    });
+});
