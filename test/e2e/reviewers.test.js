7a7090178503ea4445b0e8c33111ee141d9e858a
KLPainter <KLPainter@gmail.com>
2018-10-18 10:53:58 -0700

    compares password test failing

--- test/e2e/reviewers.test.js
+++ test/e2e/reviewers.test.js
@@ -187,7 +187,29 @@ describe('reviewers', () => {
         });
     });
 
+    it('compares passwords', () => {
+        const newReviewer = {
+            name: 'Roger Siskel',
+            company: 'At the Movies',
+            email: 'mrrogers@siskel.com',
+            clearPassword: 'movies123'
 
+        };
+        return request(app)
+            .post('/reviewers')
+            .send(newReviewer)
+            .then(newGuy => {
+                console.log('ZZZZ', newGuy.body);
+                const validPassword = newReviewer.clearPassword;
+                const invalidPassword = `${validPassword}extra`;
+
+                const validCompare = newGuy.body.compare(validPassword);
+                const invalidCompare = newGuy.body.compare(invalidPassword);
+
+                expect(validCompare).toBeTruthy();
+                expect(invalidCompare).toBeFalsy();
+            });
+    });
 
 });
 
